4f2347c7fe975fd787bd4545ef96718888c9347e
unknown <lucas_de-oliveira9@saojose.sc.senai.br>
2019-06-17 12:39:47 -0300

    aula 2

--- mod 3/aula/2/todo.js
+++ mod 3/aula/2/todo.js
@@ -1,1 +1,14 @@
- alert("deu certo!")
+var listElement =
+    document.querySelector("#app ul");
+var inputElement =
+    document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = [
+    'Google',
+    'estudar js',
+    'acessar comunidade'
+
+];  
+
+

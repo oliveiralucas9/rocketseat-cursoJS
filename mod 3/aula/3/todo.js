47b0e68f4eaf86063f0a0edd7bef0a2204ec9623
unknown <lucas_de-oliveira9@saojose.sc.senai.br>
2019-06-19 12:17:55 -0300

    mudeio titulo

---
+++ mod 3/aula/3/todo.js
@@ -0,0 +1,14 @@
+var listElement =
+    document.querySelector("#app ul");
+var inputElement =
+    document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = [
+    'Google',
+    'estudar js',
+    'acessar comunidade'
+
+];  
+
+
